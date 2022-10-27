9f4334166a2b5f75e8070275c69c73af4b32f243
Ludmila Penkrat <ludmilapenkrat@gmail.com>
2022-10-27 23:50:18 +0300

    Add change request to CSharpOOP hometask_1

--- CSharpBasics/CSharpOOP/Program.cs
+++ CSharpBasics/CSharpOOP/Program.cs
@@ -1,7 +1,7 @@
 ﻿#region Task_1
 
-Console.WriteLine("Enter count of items(3):");
-bool isItemNumber = decimal.TryParse(Console.ReadLine(), out decimal items);
+Console.WriteLine("Enter number of items:");
+bool isItemNumber = decimal.TryParse(Console.ReadLine(), out decimal numberOfItems);
 
 Console.WriteLine("Enter item price:");
 bool isItemPriceNumber = decimal.TryParse(Console.ReadLine(), out decimal itemPrice);
@@ -9,32 +9,39 @@ bool isItemPriceNumber = decimal.TryParse(Console.ReadLine(), out decimal itemPr
 Console.WriteLine("Enter general discount:");
 bool DiscountNumber = decimal.TryParse(Console.ReadLine(), out decimal discount);
 
-decimal result = new Homework_1().countTotalPriceWithDiscount(items, itemPrice, discount);
+decimal result = new Homework_1().СountTotalPriceWithDiscount(numberOfItems, itemPrice, discount);
 Console.WriteLine($"Total sum with discount = {result}\n");
 
 #endregion
 
 #region Task_2
-Console.WriteLine("Enter three position names in one line by space:");
-string? nameOfPositions = Console.ReadLine();
+int count = 0;
+while (count < 3)
+{
 
-Console.WriteLine("Enter three price per item in one line by space:");
-string? priceForEachPositions = Console.ReadLine();
+    Console.WriteLine("Enter a position name:");
+    string? nameOfPositions = Console.ReadLine();
 
-decimal positionResult = new Homework_1().countTotalPriceWithDiscount(new Homework_1().createItemsList(nameOfPositions),
-                        new Homework_1().createItemsPriceList(priceForEachPositions), discount, items);
-Console.WriteLine($"Total resal for all position with discount = {positionResult} EUR\n");
+    Console.WriteLine("Enter price per item:");
+    decimal priceForEachPositions = decimal.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter number of items:");
+    decimal number = decimal.Parse(Console.ReadLine());
+
+    new Homework_1().CountTotalPriceWithDiscount(nameOfPositions, priceForEachPositions, discount, number);
+
+    count++;
+}
+Console.WriteLine();
 
 #endregion
 
 #region Task_3
-decimal totalResultWithoutOptionalArg = new Homework_1().countTotalPriceWithDiscount(new Homework_1().createItemsList(nameOfPositions), new Homework_1().createItemsPriceList(priceForEachPositions), discount);
-Console.WriteLine($"Total resal for all position with discount = {totalResultWithoutOptionalArg} EUR");
+new Homework_1().CountTotalPriceWithDiscount(itemName: "Pencil", itemPrice: 5.25M, discount);
+
+new Homework_1().CountTotalPriceWithDiscount(itemName: "Pen", itemPrice: 1.05M, discount, 7);
 
 Console.WriteLine();
-
-decimal totalResultWithOptionalArg = new Homework_1().countTotalPriceWithDiscount(new Homework_1().createItemsList(nameOfPositions), new Homework_1().createItemsPriceList(priceForEachPositions), discount, items);
-Console.WriteLine($"Total resal for all position with discount = {totalResultWithOptionalArg} EUR");
 #endregion
 
 #region Task_4
@@ -46,25 +53,27 @@ double sideThree = double.Parse(Console.ReadLine());
 double sideFour = double.Parse(Console.ReadLine());
 double sideFive = double.Parse(Console.ReadLine());
 
-double trianglePerimeter = new Homework_1().perimeterCalculation(sideOne, sideTwo, sideThree);
-double rectanglePerimeter = new Homework_1().perimeterCalculation(sideOne, sideTwo, sideOne, sideTwo);
-double quadrilateralPerimeter = new Homework_1().perimeterCalculation(sideOne, sideTwo, sideThree, sideFive);
-double pentagonPerimeter = new Homework_1().perimeterCalculation(sideOne, sideTwo, sideThree, sideFour, sideFive);
+double trianglePerimeter = new Homework_1().PerimeterCalculation(sideOne, sideTwo, sideThree);
+double rectanglePerimeter = new Homework_1().PerimeterCalculation(sideOne, sideTwo, sideOne, sideTwo);
+double quadrilateralPerimeter = new Homework_1().PerimeterCalculation(sideOne, sideTwo, sideThree, sideFive);
+double pentagonPerimeter = new Homework_1().PerimeterCalculation(sideOne, sideTwo, sideThree, sideFour, sideFive);
 
 Console.WriteLine($"Triangle perimeter is {trianglePerimeter}. " +
     $"\nRectangle perimeter is {rectanglePerimeter}. " +
     $"\nQuadrilateral perimeter is {quadrilateralPerimeter}." +
     $"\nPentagon perimeter is {pentagonPerimeter} ");
 
+Console.WriteLine();
+
 #endregion
 
 #region Task_5
 
-double areaCirclResult = new Homework_1().areaCalculation(2.5);
-Console.WriteLine(areaCirclResult);
+double areaCirclResult = new Homework_1().AreaCalculation(2.5);
+Console.WriteLine($"The area of circle is: {areaCirclResult}");
 
-double areaTreangelCalculation = new Homework_1().areaCalculation(new Homework_1().perimeterCalculation(sideOne, sideTwo, sideThree), sideOne, sideTwo, sideThree);
-Console.WriteLine(areaTreangelCalculation);
+double areaTreangelCalculation = new Homework_1().AreaCalculation(new Homework_1().PerimeterCalculation(sideOne, sideTwo, sideThree), sideOne, sideTwo, sideThree);
+Console.WriteLine($"The area of triangle is: {areaTreangelCalculation}");
 #endregion
 
 
@@ -73,36 +82,24 @@ public class Homework_1
 {
     public int roundValue = 2;
 
-    public decimal countTotalPriceWithDiscount(decimal items, decimal itemPrice, decimal discount)
+    public decimal СountTotalPriceWithDiscount(decimal numberOfItems, decimal itemPrice, decimal discount)
     {
-        decimal totalPrice = Decimal.Round((items * itemPrice), roundValue, MidpointRounding.AwayFromZero);
+        decimal totalPrice = Decimal.Round((numberOfItems * itemPrice), roundValue, MidpointRounding.AwayFromZero);
         decimal discountSum = Decimal.Round((totalPrice * (discount / 100)), roundValue, MidpointRounding.AwayFromZero);
         decimal totalSumWithDiscount = totalPrice - discountSum;
 
         return totalSumWithDiscount;
     }
 
-    public decimal countTotalPriceWithDiscount(string[] itemName, decimal[] itemPrice, decimal discount, decimal items=5)
+    public void CountTotalPriceWithDiscount(string itemName, decimal itemPrice, decimal discount, decimal numberOfItems = 5)
     {
-        decimal totalSumForAllPositionsWithDiscount = 0;
-        for (int i = 0; i < itemName.Length; i++)
-        {
-            for (int j = i; j < itemPrice.Length; j++)
-            {
-                if(j >i )
-                {
-                    break;
-                }
-                totalSumForAllPositionsWithDiscount += countTotalPriceWithDiscount(items, itemPrice[j], discount);
-                Console.WriteLine($"For position {itemName[i]} with position price {itemPrice[j]}EUR and count {items}pcs. minus discount {discount}% " +
-                    $"total price with discount is {countTotalPriceWithDiscount(items, itemPrice[j], discount)}EUR");
-            }
+        decimal totalSumForAllPositionsWithDiscount = СountTotalPriceWithDiscount(numberOfItems, itemPrice, discount);
 
-        }
-        return totalSumForAllPositionsWithDiscount;
+        Console.WriteLine($"For position {itemName} with position price {itemPrice}EUR and count {numberOfItems}pcs. minus discount {discount}% " +
+                    $"total price with discount is {totalSumForAllPositionsWithDiscount}EUR");
     }
 
-    public double perimeterCalculation(params double[] sideLenght)
+    public double PerimeterCalculation(params double[] sideLenght)
     {
         double perimeter = 0;
         foreach (double side in sideLenght)
@@ -112,13 +109,13 @@ public class Homework_1
         return perimeter;
     }
 
-    public double areaCalculation(double radius)
+    public double AreaCalculation(double radius)
     {
         double circleArea = Math.Pow(radius, 2) * Math.PI;
         return Math.Round(circleArea, roundValue, MidpointRounding.AwayFromZero);
     }
 
-    public double areaCalculation(double perimeter, params double[] triangleSides)
+    public double AreaCalculation(double perimeter, params double[] triangleSides)
     {
         double count = 1;
         foreach (var item in triangleSides)
@@ -129,38 +126,6 @@ public class Homework_1
 
         return Math.Round(triangelArea, roundValue, MidpointRounding.AwayFromZero);
     }
-
-    public string[] createItemsList(string positionNames)
-    {
-        string[] itemsArray = positionNames.Trim().Split(" ");
-
-        return itemsArray;
-
-    }
-    public decimal[] createItemsPriceList(string priseForEachPositions)
-    {
-        string[] itemsPriceArray = priseForEachPositions.Trim().Split(" ");
-        decimal[] itemPriceArray = new decimal[itemsPriceArray.Length];
-
-        for (int i = 0; i < itemsPriceArray.Length; i++)
-        {
-            for (int j = i; j < itemsPriceArray.Length; j++)
-            {
-                if (j > i)
-                {
-                    break;
-                }
-                itemPriceArray[j] = decimal.Parse(itemsPriceArray[i]);
-            }
-        }
-
-        return itemPriceArray;
-
-    }
-
-
-
-
 }
 
 /* Инкапсуляция - любой гаджет, который мы используем (робот-пылесос). Ты знаешь как включить, куда нажать и что он делает.
