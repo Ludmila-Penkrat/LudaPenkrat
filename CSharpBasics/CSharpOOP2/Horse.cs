8d39e6931a0c21acff7d6c59e4776f845c6c1c84
Ludmila Penkrat <ludmilapenkrat@gmail.com>
2022-10-31 02:14:19 +0300

    Hometask OOP 2

---
+++ CSharpBasics/CSharpOOP2/Horse.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOOP2
+{
+    public class Horse
+    {
+        public string NickName;
+        public string Color;
+        public double Age;
+        public string Breed;
+        public double TailLength;
+        public bool IsVaccinated;
+        public bool IsHealthy;
+        public int DaysNumberLastVaccination;
+        public int NumbersOfWins = 5;
+        public int _speed;
+        public bool _isReadyToCompetition;
+
+        public static string _kind = "Horse";
+
+        public static string Kind
+        {
+            get { return _kind; }
+            set { _kind = value; }
+        }
+       
+        public int JockeysNumber { get; private set; }
+        
+        public int Speed
+        {
+            set {_speed = value;}
+        }
+
+        public bool IsReadyToCompetition
+        {
+            get {return _isReadyToCompetition;}
+        }
+
+        public Horse(string nickName, string color, double age, string breed, double tailLength, bool isVaccinated, bool isHealthy, int daysNumberLastVaccination)
+        {
+            NickName = nickName;
+            Color = color;
+            Age = age;
+            Breed = breed;
+            TailLength = tailLength;
+            IsVaccinated = isVaccinated;
+            IsHealthy = isHealthy;
+            DaysNumberLastVaccination = daysNumberLastVaccination;
+        }
+
+        public Horse()
+        {
+        }
+
+        public Horse(string nickName, double age)
+        {
+            NickName = nickName;
+            Age = age;
+        }
+
+        internal void CheckVaccination(double age)
+        {
+            switch (Age)
+            {
+                case 0.5:
+                    Console.WriteLine("First vactination is required.");
+                    break;
+                case 1:
+                    Console.WriteLine("Second vactination is required.");
+                    break;
+                case 2:
+                    Console.WriteLine("Third vactination is required.");
+                    break;
+                case 3:
+                    Console.WriteLine("Fourth vactination is required.");
+                    break;
+                default: Console.WriteLine("Vactination at the request of the owner");
+                    break;
+            }
+        }
+        public void TakePartInTournaments(bool isHealthy, bool isVaccinated)
+        {
+            if (IsHealthy && IsVaccinated)
+            {
+                Console.WriteLine("The horse can take part in the races");
+            } else
+            {
+                Console.WriteLine("The horse should go to the vet");
+            }
+        }
+        public bool NeedDoVaccination(int DaysNumberLastVaccination)
+        {
+            if (DaysNumberLastVaccination >= 0 && DaysNumberLastVaccination <= 365)
+            {
+            return IsVaccinated = true;
+            } else
+            {
+                return IsVaccinated = false;
+            }
+        }
+        private string CutTail (double tailLength)
+        {
+            if (tailLength <= 0)
+            {
+                return $"Value {tailLength} is not correct";
+            } else if (tailLength >0 && tailLength <= 1.2)
+            {
+                return $"Your horse with tail {tailLength} look perfect and ready for exibition.";
+            }else 
+            {
+                return $"The horse with {tailLength} should go to a haidresser for animals.";
+            }
+        }
+        
+        public void PrintParticipantGreeting(string name, string breed)
+        {
+            Console.WriteLine($"Welcome the next participant of tournament {name} of the {breed} breed");
+        }
+
+        public static string CheckHorseTrainigOrCompetition(int jokeyNumber)
+        {
+            switch (jokeyNumber)
+            {
+                case 0:
+                    return "This horse is too small for training or competition";
+                case 1:
+                    return "This horse is for competition";
+                default:
+                    return "This horse is for training in the arena";
+            }
+
+        }
+    }
+}
---
+++ CSharpBasics/CSharpOOP2/Program.cs
@@ -0,0 +1,110 @@
+﻿using CSharpOOP2;
+
+
+#region Task_4
+Horse horse1 = new Horse();
+horse1.NickName = "Rose";
+horse1.Color = "Brown";
+horse1.Age = 2;
+horse1.Breed = "Arabic";
+horse1.TailLength = 1.2;
+horse1.IsVaccinated = true;
+horse1.IsHealthy= true;
+horse1.DaysNumberLastVaccination = 78;
+#endregion
+
+#region Task_5
+
+Horse horse2 = new Horse() { NickName = "Blansh", Color = "White", Age = 1, Breed = "Belarusian harness", TailLength = 1.0, IsVaccinated = true, IsHealthy = true, DaysNumberLastVaccination = 155 };
+
+#endregion
+
+#region Task_9
+
+Horse horseAllFields = new Horse("Grosh", "Black", 2.5, "Belgian", 0.3, true, true, 275);
+
+#endregion
+
+#region Task_10
+
+bool result = horse1.NeedDoVaccination(375);
+Console.WriteLine(result);
+horse1.TakePartInTournaments(result, horse1.IsVaccinated);
+
+#endregion
+
+#region Task_11
+
+// with internal modifier
+horse2.CheckVaccination(horse2.Age);
+
+// with private modifier
+//horse2.CutTail(horse2.TailLength); Error Line
+
+#endregion
+
+#region Task_12
+
+Horse horseWithoutFields = new Horse();
+//horseWithoutFields.NickName = "Rose";
+horseWithoutFields.Color = "Brown";
+//horseWithoutFieldAge.Age = 2;
+//horseWithoutFields.Breed = "Arabic";
+horseWithoutFields.TailLength = 1.2;
+//horseWithoutFieldAge.IsVaccinated = true;
+//horseWithoutFieldAge.IsHealthy = true;
+horseWithoutFields.DaysNumberLastVaccination = 78;
+
+horseWithoutFields.CheckVaccination(horseWithoutFields.Age); //this method is executed without error because the default value for double = 0.
+horseWithoutFields.TakePartInTournaments(horseWithoutFields.IsVaccinated, horseWithoutFields.IsHealthy); //this method is executed without error because the default value for bool is false
+horseWithoutFields.PrintParticipantGreeting(horseWithoutFields.NickName, horseWithoutFields.Breed); // this method is executed without error but with the empty valu for missing parameters (Welcome the next participant of tournament <> of the <>  breed)
+
+#endregion
+
+#region Task_13
+
+Horse horsDefaultValueAge = new Horse();
+horsDefaultValueAge.NickName = "Brige";
+Console.WriteLine($"There is new instanse of Horse class with nickname {horsDefaultValueAge.NickName} has {horsDefaultValueAge.NumbersOfWins} wins.");
+
+Console.WriteLine();
+#endregion
+
+#region Task_15
+Horse horseChechProperty = new Horse();
+horseChechProperty.Speed = 15;
+// Console.WriteLine(horseChechProperty.Speed); Error Line
+
+// horseChechProperty.IsReadyToCompetition = true;  Error Line
+Console.WriteLine($"This horse can take part in competition: if {horseChechProperty.IsReadyToCompetition} = true - \"Yes\" if {horseChechProperty.IsReadyToCompetition} = false - \"No\"");
+
+Console.WriteLine();
+#endregion
+
+#region Task_17
+Console.WriteLine("Use static propertiy");
+
+Console.WriteLine($"Kind of our animal is {Horse.Kind}");
+Horse.Kind = "New Horse";
+Console.WriteLine($"Now we change animal to {Horse.Kind}");
+
+Console.WriteLine();
+#endregion
+
+#region Task_18
+Console.WriteLine("Enter number of jockeys");
+int numberOfJockeys = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Horse.CheckHorseTrainigOrCompetition(numberOfJockeys));
+Console.WriteLine();
+#endregion
+
+#region Task_20
+HorseHelper.PrintBasicHorseDescription(horse1);
+
+HorseHelper.AddorChangeColor(horsDefaultValueAge, "Blue");
+HorseHelper.PrintBasicHorseDescription(horsDefaultValueAge);
+
+Console.WriteLine($"Age of horse is {HorseHelper.CalculateHourseAgeByTailLength(horse2)}");
+
+#endregion
