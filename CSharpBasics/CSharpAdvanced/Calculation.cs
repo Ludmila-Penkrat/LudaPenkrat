9ecd8ce6b70cea24f1a1981bf73150823a825d66
Ludmila Penkrat <ludmilapenkrat@gmail.com>
2022-11-23 20:34:53 +0300

    CSharpAdvanced Part1 Hometask

---
+++ CSharpBasics/CSharpAdvanced/Calculation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpAdvanced
+{
+    public static class Calculation
+    {
+        public static decimal TotalPrice<T>(T flowers, int count) where T : Flower
+        {
+            decimal titalPrise = count * flowers.PricePerItem;
+            return titalPrise;
+        }
+
+        public static string GetCheck<T>(T flowers, int count) where T : Flower
+        {
+            return new StringBuilder()
+                .Append(flowers.ToString())
+                .Append($"\nTotal count: {count}")
+                .Append($"\nTotal price: {TotalPrice(flowers, count)}")
+                .ToString();
+        }
+    }
+}
---
+++ CSharpBasics/CSharpAdvanced/Flower.cs
@@ -0,0 +1,27 @@
+﻿
+namespace CSharpAdvanced
+{
+    public class Flower
+    {
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+
+        public decimal PricePerItem { get; set; }
+        
+        public Flower(string name, string color, decimal pricePerItem)
+        {
+            Name = name;
+            Color = color;
+            PricePerItem = pricePerItem;
+        }
+
+        public override string ToString()
+        {
+            return $"\n{GetType().Name}" + 
+                $"\nSort: {Name}" +
+                $"\nColor: {Color}" +
+                $"\nPrice per item: {PricePerItem}";
+        }
+    }
+}
