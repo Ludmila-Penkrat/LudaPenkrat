a017061398083eef38ae907edb006e02a149e65e
Ludmila Penkrat <ludmilapenkrat@gmail.com>
2022-12-18 23:12:00 +0300

    Add any changes to selenium_test

--- CSharpBasics/SeleniumTest/NUnitTests.cs
+++ CSharpBasics/SeleniumTest/NUnitTests.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace SeleniumTest
 {
+    [TestFixture]
     internal class NUnitTests
     {
+        [Test]
+        public void ChechThrowExceptionWithNegativeValueTest()
+        {
+            var baseSide = -10;
+            var firstSide = -20;
+            var secondSide = -20;
+                       
+        }
     }
 }
